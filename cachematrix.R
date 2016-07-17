c71710656d7ec164e5975fe945eb413411f4f183
Vijayakumar Ramachandran <vijayakumar.ramachandran@gmail.com>
2016-07-17 18:19:07 +0530

    Update cachematrix.R
    This is a part of the assignment. Had been following the basis of mean function example. It is took a while to understand the working of this.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
+#makeCacheMatrix is a function taking square matrix as input.
+#set up the matrix, gets the matrix, 
+#set up the inverse of the matrix, gets the inverse of the matrix
+#on the lines of how  prof had explained the mean function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invm <- NULL
+  set <- function(y) {
+    x <<- y
+    invm <<- NULL
+  }
+  get <- function() x
+  setxbar <- function(xbar) invm <<- xbar
+  getxbar <- function() invm
+  list(set = set, get = get,
+       setxbar = setxbar,
+       getxbar = getxbar)
 }
 
 
 ## Write a short comment describing this function
+# Whenever a inverse is sought, we can use the below function to check if the
+#inverse exists in the cache, rather than solving all over again using Solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x', which is xbar.
+  invm <- x$getxbar()
+  if(!is.null(invm)) {
+    message("getting cached data")
+    return(invm)
+  }
+  mydata <- x$get()
+  invm <- solve(mydata, ...)
+  x$setxbar(invm)
+  return(invm)
 }
